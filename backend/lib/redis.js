13fe48f95fa1571e34d84356c24e867a8f36ddf4
LoiJenTECH3 <loijentech3@gmail.com>
2025-11-30 22:43:40 +0800

    Update redis.js

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -8,5 +8,3 @@ export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
 
 
 
-//const client = new Redis("rediss://default:AS_wAAIncDIxNDliMGJiYjEzOGY0MjAyOWU0YTUwYzBjNzhmMTY2NXAyMTIyNzI@big-duckling-12272.upstash.io:6379");
-//await redis.set("foo", "bar");
