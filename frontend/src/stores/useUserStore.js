0df7c8366c60d8916de39f48774ece051a38077f
LoiJenTECH3 <loijentech3@gmail.com>
2025-12-01 20:14:10 +0800

    Update useUserStore.js

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -62,7 +62,7 @@ export const useUserStore = create((set, get) => ({
 
 		set({ checkingAuth: true });
 		try {
-			const response = await axios.post("/auth/refresh-token");
+			const response = await axios.post("/api/auth/refresh-token");
 			set({ checkingAuth: false });
 			return response.data;
 		} catch (error) {
