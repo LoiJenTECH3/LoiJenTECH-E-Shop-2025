48f6b29bc1a3378429f587efcf08fa78e415fee8
LoiJenTECH3 <loijentech3@gmail.com>
2025-12-04 12:53:14 +0800

    Update useProductStore.js

--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -16,7 +16,7 @@ export const useProductStore = create((set) => ({
 				loading: false,
 			}));
 		} catch (error) {
-			toast.error(error.response.data.error);
+			toast.error(error.response?.data?.error);
 			set({ loading: false });
 		}
 	},
@@ -27,7 +27,7 @@ export const useProductStore = create((set) => ({
 			set({ products: response.data.products, loading: false });
 		} catch (error) {
 			set({ error: "Failed to fetch products", loading: false });
-			toast.error(error.response.data.error || "Failed to fetch products");
+			toast.error(error.response?.data?.error || "Failed to fetch products");
 		}
 	},
 	fetchProductsByCategory: async (category) => {
@@ -37,7 +37,7 @@ export const useProductStore = create((set) => ({
 			set({ products: response.data.products, loading: false });
 		} catch (error) {
 			set({ error: "Failed to fetch products", loading: false });
-			toast.error(error.response.data.error || "Failed to fetch products");
+			toast.error(error.response?.data?.error || "Failed to fetch products");
 		}
 	},
 	deleteProduct: async (productId) => {
@@ -66,7 +66,7 @@ export const useProductStore = create((set) => ({
 			}));
 		} catch (error) {
 			set({ loading: false });
-			toast.error(error.response.data.error || "Failed to update product");
+			toast.error(error.response?.data?.error || "Failed to update product");
 		}
 	},
 	fetchFeaturedProducts: async () => {
